d4b42d7387efdb05e8cd3e5b689b9b3a05fc5eae
Sahaj Asim <mrx6909@gmail.com>
2026-03-15 23:54:52 +0530

    started react router dom

---
+++ 18-react-router-dom/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+const App = () => {
+  return (
+    <div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+    </div>
+  )
+}
+
+export default App
